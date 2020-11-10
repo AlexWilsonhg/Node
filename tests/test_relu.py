61c23854dd2d55334a9dc2363991cc0c5e3859da
Alex Wilson <awilson.hg@gmail.com>
2020-11-10 14:01:02 -0500

    flesh out relu test

--- tests/test_relu.py
+++ tests/test_relu.py
@@ -5,18 +5,31 @@ from nodes.Relu import Relu
 
 class TestRelu(unittest.TestCase):
 
-	def test_relu(self):
+	def test_single_value(self):
 
 		relu = Relu()
 
-		indata = np.array([0,1,1])
+		indata = 1
 		outdata = relu.compute(indata)
-		self.assertTrue(np.array_equal(outdata, [0,1,1]))
+		self.assertTrue(np.array_equal(outdata, 1))
 
-		indata = np.array([-1,0])
-		outdata = relu.compute(indata)
-		self.assertTrue(np.array_equal(outdata, [0,0]))
+	def test_vector(self):
 
-		indata = np.array([5000,-5000])
+		relu = Relu()
+
+		indata = np.array([-1,0,500])
 		outdata = relu.compute(indata)
-		self.assertTrue(np.array_equal(outdata, [5000,0]))
+		self.assertTrue(np.array_equal(outdata, [0,0,500]))
+
+	def test_2d_matrix(self):
+
+		relu = Relu()
+
+		indata = np.array([[5000,-5000],
+						   [0   ,    0],
+						   [-100,  500]])
+
+		outdata = relu.compute(indata)
+		self.assertTrue(np.array_equal(outdata, np.array([[5000,	0],
+														  [0,	    0],
+														  [0,	  500]])))
